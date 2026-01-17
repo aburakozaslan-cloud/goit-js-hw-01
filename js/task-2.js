b0bfdb66608a1021e1789bdbe589854511e5b7e0
Abdullah Burak Özaslan <abdullahburak564@gmail.com>
2026-01-17 23:17:27 +0300

    Fix typo in totalPrice calculation

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@
 
 function getShippingMessage(country, price, deliveryFee) {
     
-    const totalPrice= price + deliveryFree;
+    const totalPrice= price + deliveryFee;
 
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 
